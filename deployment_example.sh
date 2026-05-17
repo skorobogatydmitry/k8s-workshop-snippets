cac3e22f0fa9519a72694ebdd014f31046670007
Skorobogaty Dmitry <skorobogaty.dmitry@gmail.com>
2026-05-17 16:58:17 +0400

    improve snippets some more

--- deployment_example.sh
+++ deployment_example.sh
@@ -1,5 +1,6 @@
 kubectl create deployment just-nginx --image=nginx:alpine --replicas=2
 kubectl expose deployment just-nginx --port=80
-kubectl run observer --image=alpine -it -- sh`, `apk add curl; nslookup just-nginx; curl just-nginx
+kubectl run observer --image=alpine -it -- sh
+apk add curl; nslookup just-nginx; curl just-nginx
 kubectl logs deploy/just-nginx
 kubectl get service/just-nginx -o yaml
--- install_cni.sh
+++ install_cni.sh
@@ -2,7 +2,7 @@ curl -LO https://github.com/flannel-io/flannel/releases/latest/download/kube-fla
 sed -i 's#10.244.0.0/16#172.16.66.0/24#g' kube-flannel.yml
 sed -i 's#"EnableNFTables": false#"EnableNFTables": true#g' kube-flannel.yml
 sed -Ei 's#"type": "portmap",#\0 "backend": "nftables",#' kube-flannel.yml
-dnf install -y nftables ; systemctl status nftables
+sudo dnf install -y nftables ; sudo systemctl status nftables
 kubectl apply -f kube-flannel.yml
 kubectl edit -n kube-flannel cm/kube-flannel-cfg; rm -rf /var/lib/cni/networks/* ; reboot
 kubectl -n kube-flannel get daemonset.apps/kube-flannel-ds
