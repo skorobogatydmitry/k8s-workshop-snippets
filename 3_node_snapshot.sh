38923cde97f68d36bc25ba10f6ae4be4c4241651
Skorobogaty Dmitry <skorobogaty.dmitry@gmail.com>
2026-05-20 17:09:56 +0400

    add snaphot to the 2nd chapter

---
+++ 3_node_snapshot.sh
@@ -0,0 +1,3 @@
+virsh snapshot-create-as --name 3-node-cluster-non-ha --domain k8s-node1
+virsh snapshot-create-as --name 3-node-cluster-non-ha --domain k8s-node2
+virsh snapshot-create-as --name 3-node-cluster-non-ha --domain k8s-node3
